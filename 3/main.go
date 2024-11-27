7d8e85019d486f103b369f5d5220eca7a2bb713c
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-11-27 09:39:12 +0800

    up

--- 3/main.go
+++ 3/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type TokenType struct {
+
+}
+
+type Token struct {
+	Value     string
+	TokenType
+}
+
 func main() {
 	fmt.Println("advent of code 2023 day 3")
+}
+
+func getTokens() {
+
 }
